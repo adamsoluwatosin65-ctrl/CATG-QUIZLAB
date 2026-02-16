0ea54db28b6d6f3bc846e8e2049dd1534ee628c0
adamsoluwatosin65-ctrl <adamsoluwatosin65@gmail.com>
2026-02-16 21:55:49 +0000

    Update bible_trivia.py

--- bible_trivia.py
+++ bible_trivia.py
@@ -1,5 +1,8 @@
 # --- LOBBY PAGE WITH AUTO-UPDATE ---
 import streamlit as st
+if 'page' not in st.session_state:
+    st.session_state.page = 'welcome'
+
 if st.session_state.get('page') == 'lobby':
     st.markdown(f"<h2 style='text-align: center; color: white;'>Lobby: {st.session_state.room_code}</h2>", unsafe_allow_html=True)
     
@@ -35,6 +38,7 @@ if st.session_state.get('page') == 'lobby':
 
     # Execute the fragment
     lobby_sync()
+
 
 
 
